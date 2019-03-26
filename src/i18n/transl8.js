d3024ef991f4d832875ec55ab448e555ef1722dc
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2019-03-26 20:06:58 +0100

    Use local js files for storing translations
    See #10700

--- src/i18n/transl8.js
+++ src/i18n/transl8.js
@@ -6,31 +6,18 @@
  *
  * @author: Daniel de Oliveira
  * @author: Jan G. Wieners
+ * @author: Sebastian Cuy
  */
 angular.module('idai.components')
-    .factory('transl8', ['$http', '$location', '$sce', 'language', 'componentsSettings',
-        function ($http, $location, $sce, language, componentsSettings) {
+    .factory('transl8', ['$location', 'language', 'componentsSettings',
+        function ($location, language, componentsSettings) {
 
             var lang = language.currentLanguage(),
-                translationsLoaded = false,
-                transl8Url, res, promise, i, translations = {}; // Map: [transl8_key,translation]
+                i, translations = {}; // Map: [transl8_key,translation]
 
             if (['de', 'en'].indexOf(lang) === -1) lang = 'en';
 
-            transl8Url = componentsSettings.transl8Uri.replace('{LANG}', lang);
-
-            promise = $http.jsonp($sce.trustAsResourceUrl(transl8Url)).then(function (result) {
-
-                res = result.data;
-                i = res.length;
-
-                for (i; i--;) {
-                    translations[res[i].key] = res[i].value;
-                }
-                translationsLoaded = true;
-            }).catch(function (error) {
-                console.error("ERROR: Could not get translations.");
-            });
+            translations = transl8[lang];
 
             return {
 
@@ -42,7 +29,6 @@ angular.module('idai.components')
                  *   there is no translation for the given key.
                  */
                 getTranslation: function(key) {
-                    if (!translationsLoaded) return key;
 
                     var translation = translations[key];
                     if (!translation || 0 === translation.length) {
@@ -52,7 +38,7 @@ angular.module('idai.components')
                 },
 
                 onLoaded: function () {
-                    return promise;
+                    return Promise.resolve();
                 }
 
             }
